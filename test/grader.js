57279746761322dd1aa5ac614d53959af11b4f8c
howzlife <nico_dubus@hotmail.com>
2013-07-16 14:05:14 +0000

    updated grader.js

--- test/grader.js
+++ test/grader.js
@@ -24,16 +24,14 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var rest = require('restler');
-var sys = require('util');
-
+var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
-	console.log("%s does not exist. Exiting.", instr);
-	process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+        console.log("%s does not exist. Exiting.", instr);
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
 };
@@ -42,35 +40,19 @@ var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
 
-var cheerioURL = function(htmlurl) {
-    return cheerio.load(htmlurl);
-};
-
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-// takes in a string of HTML and a checks file and tests it
-var checkHtml = function(html, checksfile) {
-    $ = html;
+var checkHtmlFile = function(htmlfile, checksfile) {
+    $ = cheerioHtmlFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
-	var present = $(checks[ii]).length > 0;
-	out[checks[ii]] = present;
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
     }
     return out;
-};
-
-var processFormat = function(htmlfile, url, checksfile) {
-    if (!htmlfile) { // we are using url
-	rest.get(url).on('complete', function(result) {
-	        console.log(checkHtml(cheerioURL(result), checksfile));
-	        return checkHtml(cheerioURL(result), checksfile);
-	    });
-    } else {
-	return checkHtml(cheerioHtmlFile(htmlfile), checksfile);
-    }
 };
 
 var clone = function(fn) {
@@ -81,13 +63,12 @@ var clone = function(fn) {
 
 if(require.main == module) {
     program
-    .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-    .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists))
-    .option('-u, --url <url>', 'URL to check')
-    .parse(process.argv);
-    var checkJson = processFormat(program.file, program.url, program.checks);
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .parse(process.argv);
+    var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
-    exports.checkHtml = checkHtml;
+    exports.checkHtmlFile = checkHtmlFile;
 }
